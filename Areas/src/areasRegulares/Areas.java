88e193558c612bbfde2eb1a01bf99800d69e1fd8
micha <micha@LAPTOP-D1Q8F36U>
2025-03-21 19:34:25 +0100

    declarar variables base y altura, asignamos valores.

--- Areas/src/areasRegulares/Areas.java
+++ Areas/src/areasRegulares/Areas.java
@@ -4,6 +4,9 @@ public class Areas {
 
 	public static void main(String[] args) {
 		//Programa para calcular areas
+		//declarar variables base y altura, asignamos valores.
+		int base=5;
+		int altura=10;
 		
 		
 	}
